452f184435003896d83d6fc26cf4aef3c985b365
Suresh <suresh.wrc@gmail.com>
2023-01-11 15:55:59 -0500

    Add feature extraction code

--- predict.py
+++ predict.py
@@ -3,6 +3,11 @@
       Email   : sureshp@mtu.edu
 """
 
+
+"""
+import required libraries
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -10,55 +15,62 @@ import seaborn as sns
 import xgboost as xgb
 from Bio import SeqIO
 from keras import backend as K
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.manifold import TSNE
 from sklearn.metrics import accuracy_score, confusion_matrix, matthews_corrcoef
-from sklearn.svm import SVC
-from sklearn.utils import shuffle
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import (Conv1D, Dense, Dropout, Flatten, Input,
-                                     LeakyReLU, MaxPooling1D, Reshape)
-from tensorflow.keras.losses import BinaryCrossentropy
+
 from tensorflow.keras.models import Model, Sequential, load_model
 from tensorflow.keras.optimizers import Adam
 from tqdm import tqdm
 
-# import feature extraction code
+# for ProtT5 model
+import torch
+from transformers import T5EncoderModel, T5Tokenizer
+import re
+import gc
 
-# File paths
+"""
+define file paths and other parameters
+"""
 input_fasta_file = "input/sequence.fasta" # load test sequence
 output_csv_file = "output/results.csv" 
 model_path = 'models/pLMSNOSite.h5'
 win_size = 37
 
-######################## Feature Extraction Using ProtT5################
+
+"""
+Load tokenizer and pretrained model ProtT5
+"""
+# install SentencePiece transformers if not installed already
 #!pip install -q SentencePiece transformers
-import torch
-from transformers import T5EncoderModel, T5Tokenizer
-import re
-import os
-import requests
-from tqdm.auto import tqdm
-import tqdm
-from Bio import SeqIO
-import gc
-import pandas as pd
-import numpy as np
+
 
 tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50", do_lower_case=False )
 pretrained_model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50")
 # pretrained_model = pretrained_model.half()
 gc.collect()
 
+# define devices
 # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
 pretrained_model = pretrained_model.to(device)
 pretrained_model = pretrained_model.eval()
 
-def get_protT5_features(sequences_Example): 
-    sequences_Example = [' '.join(e) for e in sequences_Example]
-    sequences_Example = [re.sub(r"[UZOB]", "X", sequence) for sequence in sequences_Example]
-    ids = tokenizer.batch_encode_plus(sequences_Example, add_special_tokens = True, padding = True)
+def get_protT5_features(sequence): 
+    """
+    Description: Extract a window from the given string at given position of given size
+                (Need to test more conditions, optimizations)
+    Input:
+        sequence (str): str of length l
+    Returns:
+        tensor: l*1024
+    """
+    # add space in between amino acids
+    sequence = [' '.join(e) for e in sequence]
+    
+    # replace rare amino acids with X
+    sequence = [re.sub(r"[UZOB]", "X", seq) for seq in sequence]
+    
+    # set configurations and extract features
+    ids = tokenizer.batch_encode_plus(sequence, add_special_tokens = True, padding = True)
     input_ids = torch.tensor(ids['input_ids']).to(device)
     attention_mask = torch.tensor(ids['attention_mask']).to(device)
     
@@ -70,7 +82,7 @@ def get_protT5_features(sequences_Example):
     seq_emd = embedding[0][: seq_len - 1]
     
     return seq_emd
-########################################################################
+    
 
 def extract_one_windows_position(sequence,site,window_size=37):
     '''
@@ -126,7 +138,7 @@ ProtT5_model = load_model('models/ProtT5.h5', compile = False)
 Embedding_model = load_model('models/Embedding.h5', custom_objects={"K": K}, compile = False)
 
 
-for seq_record in tqdm.tqdm(SeqIO.parse(input_fasta_file, "fasta")):
+for seq_record in tqdm(SeqIO.parse(input_fasta_file, "fasta")):
     prot_id = seq_record.id
     sequence = seq_record.seq
     
